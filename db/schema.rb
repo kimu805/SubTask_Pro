caa3f1c58161ea9067268fcce0f099d02134ea1d
kimu805 <kimukimuok805@gmail.com>
2024-12-21 18:37:21 +0900

    create priorities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_16_081533) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_21_093604) do
+  create_table "priorities", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_priorities_on_user_id"
+  end
+
   create_table "tasks", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
@@ -36,5 +44,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_081533) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "priorities", "users"
   add_foreign_key "tasks", "users"
 end
