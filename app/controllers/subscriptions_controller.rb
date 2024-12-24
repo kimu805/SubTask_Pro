f472f31eed3f67dbf88318729e363dfa3dc2940a
kimu805 <kimukimuok805@gmail.com>
2024-12-24 12:14:07 +0900

    Update subscriptions_controller.rb

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,26 @@
 class SubscriptionsController < ApplicationController
+  def new
+  end
+
+  def create
+    customer = Stripe::Customer.create(email: current_user.email, source: params[:stripeToken])
+    subscription = Stripe::Subscription.create({
+      customer: customer.id,
+      items: [ { price: params[:price_id] } ]
+    })
+    current_user.subscription.create(
+      stripe_subscription_id: subscription.id,
+      status: subscription.status
+    )
+    redirect_to root_path, notice: "サブスクリプションを開始しました"
+  end
+
+  def destroy
+    subscription = Stripe::Subscription.retrieve(current_user.subscription.stripe_subscription_id)
+    subscription.cancel_at_period_end = true
+    subscription.save
+
+    current_user.subscription.update(status: "canceled")
+    redirect_to root_path, notice: "サブスクリプションを取消しました"
+  end
 end
